b30b926845d8a976585a41ba511d76ed8f21a810
M Mundrawala <mundrawala18@gmail.com>
2022-02-25 18:11:00 +0530

    gj

---
+++ js/753.bad7f88c.js
@@ -0,0 +1,6 @@
+"use strict";(self["webpackChunkA22_refer_app"]=self["webpackChunkA22_refer_app"]||[]).push([[753],{4753:function(e,n,t){t.r(n),t.d(n,{mdTransitionAnimation:function(){return r}});var i=t(5980),a=t(5026);
+/*!
+ * (C) Ionic http://ionicframework.com - MIT License
+ */
+const r=(e,n)=>{const t="40px",r="0px",o="back"===n.direction,s=n.enteringEl,c=n.leavingEl,l=(0,a.g)(s),d=l.querySelector("ion-toolbar"),u=(0,i.c)();if(u.addElement(l).fill("both").beforeRemoveClass("ion-page-invisible"),o?u.duration(n.duration||200).easing("cubic-bezier(0.47,0,0.745,0.715)"):u.duration(n.duration||280).easing("cubic-bezier(0.36,0.66,0.04,1)").fromTo("transform",`translateY(${t})`,`translateY(${r})`).fromTo("opacity",.01,1),d){const e=(0,i.c)();e.addElement(d),u.addAnimation(e)}if(c&&o){u.duration(n.duration||200).easing("cubic-bezier(0.47,0,0.745,0.715)");const e=(0,i.c)();e.addElement((0,a.g)(c)).onFinish((n=>{1===n&&e.elements.length>0&&e.elements[0].style.setProperty("display","none")})).fromTo("transform",`translateY(${r})`,`translateY(${t})`).fromTo("opacity",1,0),u.addAnimation(e)}return u}}}]);
+//# sourceMappingURL=753.bad7f88c.js.map
---
+++ js/78.f08a9ce2.js
@@ -0,0 +1,6 @@
+"use strict";(self["webpackChunkA22_refer_app"]=self["webpackChunkA22_refer_app"]||[]).push([[78],{6078:function(e,t,n){n.r(t),n.d(t,{startStatusTap:function(){return s}});var r=n(3087),o=n(6587);
+/*!
+ * (C) Ionic http://ionicframework.com - MIT License
+ */
+const s=()=>{const e=window;e.addEventListener("statusTap",(()=>{(0,r.wj)((()=>{const t=e.innerWidth,n=e.innerHeight,s=document.elementFromPoint(t/2,n/2);if(!s)return;const c=s.closest("ion-content");c&&new Promise((e=>(0,o.c)(c,e))).then((()=>{(0,r.Iu)((async()=>{c.style.setProperty("--overflow","hidden"),await c.scrollToTop(300),c.style.removeProperty("--overflow")}))}))}))}))}}}]);
+//# sourceMappingURL=78.f08a9ce2.js.map
